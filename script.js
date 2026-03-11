45e170afaf008076542e7060eeca333fe8abed90
Yamini-Soni <mv9497y94@gmail.com>
2026-03-11 17:53:27 +0530

    updated portfolio design

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
+let recognition;
+let voiceActive=false;
+
+function showSection(id){
+
+document.querySelectorAll(".section").forEach(sec=>{
+sec.classList.remove("active");
+});
+
+document.getElementById(id).classList.add("active");
+
+}
+
 function startVoice(){
 
-const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+if(voiceActive) return;
+
+recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+recognition.continuous = true;
+recognition.lang = "en-US";
+
+voiceActive=true;
 
 recognition.start();
 
-recognition.onresult = function(event){
+recognition.onresult=function(event){
 
-let command = event.results[0][0].transcript.toLowerCase();
+let command = event.results[event.results.length-1][0].transcript.toLowerCase();
 
-if(command.includes("about"))
-location.hash = "#about";
+if(command.includes("home")) showSection("home");
 
-else if(command.includes("skills"))
-location.hash = "#skills";
+else if(command.includes("about")) showSection("about");
 
-else if(command.includes("projects"))
-location.hash = "#projects";
+else if(command.includes("skills")) showSection("skills");
 
-else if(command.includes("contact"))
-location.hash = "#contact";
+else if(command.includes("projects")) showSection("projects");
+
+else if(command.includes("contact")) showSection("contact");
 
 };
+
+}
+
+function stopVoice(){
+
+if(recognition){
+
+recognition.stop();
+voiceActive=false;
+
+}
+
 }
